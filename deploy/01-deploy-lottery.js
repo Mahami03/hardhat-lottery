77675f6422c35d2881cfddd2d1febb891f162c86
Mahammi <muhammadabdulqadir03@gmail.com>
2023-11-30 14:22:34 +0300

    firssss

---
+++ deploy/01-deploy-lottery.js
@@ -0,0 +1,47 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../helper-hardhat-config")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("1")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainID = network.config.chainId
+    let vrfCoordinatorV2Address, subscriptionId
+
+    if (developmentChains.includes(network.name)) {
+        const vrfCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock")
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.target
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+        const transactionReciept = await transactionResponse.wait(1)
+        subscriptionId = transactionReciept.events[0].args.subId
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, VRF_SUB_FUND_AMOUNT)
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainID]["vrfCoordinatorV2"]
+        subscriptionId = networkConfig[chainID]["subscriptionId"]
+    }
+
+    const entranceFee = networkConfig[chainID]["entranceFee"];
+    const keyHash = networkConfig[chainID]["keyHash"]
+    const callbackGasLimit = networkConfig[chainID]["callbackGasLimit"]
+    const interval = networkConfig[chainID]["interval"]
+
+    const argument = [vrfCoordinatorV2Address, entranceFee, keyHash, subscriptionId, callbackGasLimit, interval]
+
+    const lottery = await deploy("Lottery", {
+        from: deployer,
+        args: argument,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(lottery.address, args)
+    }
+    log("-----------------------------------")
+
+    module.exports.tags = ["all", "lottery"]
+
+}
